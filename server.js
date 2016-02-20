b6cbff270276405f3b8000a908c501bf42b1d40a
fresh5447 <doug@kosmojo.com>
2016-02-20 16:47:14 -0700

    new user registration configured

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var passport = require('passport');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/2blog');
+var flash = require('connect-flash');
+var session = require('express-session');
 
 var blogRoutes = require('./routes/blog');
+var userRoutes = require('./routes/user');
 var commentRoutes = require('./routes/comment');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+//Passport stuff..
+
+app.use(session({
+  secret: 'ilovescotchscotchyscotchscotch'
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(session({
+  cookie: {
+    maxAge: 60000
+  }
+}));
+app.use(flash());
+
+require('./config/passport')(passport);
+// routes ======================================================================
+require('./routes/user.js')(app, passport);
 
 app.use(function(req, res, next){
   console.log("Atleast I am trying to do something...")
